cfc04fe96a1f99f8f4a74818664ac59852ce35ce
root <vctrams@vctrams@gmail.com>
2023-10-19 07:37:25 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,8 +8,9 @@
 
 void print_alphabet(void)
 {
-	char character;
-	for (character = 'a'; character <= 'z'; character++)
-	       _putchar(character);
-_putchar('\n');
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
 }
