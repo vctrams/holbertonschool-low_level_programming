0e194ebd36637f31a61f44002e851ce169060689
root <vctrams@vctrams@gmail.com>
2023-10-24 08:26:13 -0700

    98 Battery st.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter and updates t
+ * @n: - the value to be updated to 98
+ * Return: Always 0
+ */
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+
+
+*n = 98;
+}
