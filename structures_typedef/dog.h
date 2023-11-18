1ed27e61bffdc018814faff2652db934492554e0
root <vctrams@vctrams@gmail.com>
2023-11-17 16:12:12 -0800

    Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * name, type = char *
+ * age, type = float
+ * owner, type = char *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
