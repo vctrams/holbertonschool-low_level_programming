e7ac335c8404a13a1494f26fbc012644672c7183
root <vctrams@vctrams@gmail.com>
2023-12-07 14:06:31 -0800

    1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * list_len - show the number of elements of a list
+ * @h: linked list
+ *
+ * Return: number of elements of a list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
