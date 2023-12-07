6deb56be5c4e8e60dcef3be5c9d048404358e04e
root <vctrams@vctrams@gmail.com>
2023-12-07 14:29:15 -0800

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -34,7 +34,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		while (current_node->next != NULL)
 			current_node = current_node->next;
-		current_node->next + new_node;
+		current_node->next = new_node;
 	}
 
 	return (*head);
