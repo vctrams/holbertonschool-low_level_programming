321504e0136733354364f34c17c2a45bec84e410
root <vctrams@vctrams@gmail.com>
2023-12-21 09:00:18 -0800

    0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * binary_to_uint - converts a binary number to an unsigned int
+  * @b: binary number
+  * Return: the converted number or 0 if it fails
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, mult = 1;
+	int len;
+
+	if (!b)
+		return (0);
+
+	for (len = 0; b[len];)
+		len++;
+
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		num += (b[len] - '0') * mult;
+		mult *= 2;
+	}
+
+	return (num);
+}
